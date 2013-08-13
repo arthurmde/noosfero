5940f7b57eee9e0c5a42d8b15813239db020ddae
Luiz Matos + Arthur Del Esposte + Fabio Teixeira + Daniel Bucher <luizff.matos@gmail.com>
2013-08-13 16:39:26 -0300

    Adding academic info section into ProfileController#index

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -28,6 +28,24 @@ module ProfileHelper
     end
   end
 
+  def display_academic_info(profile)
+    lattes = display_field(_('Lattes:'), profile, :lattes) { |lattes| link_to 'Lattes', lattes, :target => 'blank' }
+    fields_of_interest = display_field(_('Fields of Interest:'), profile, :fields_of_interest)
+    schooling = display_field(_('Schooling:'), profile, :schooling)
+    formation = display_field(_('Formation:'), profile, :formation)
+    area_of_study = display_field(_('Area of study:'), profile, :custom_area_of_study)
+    if area_of_study.blank?
+      area_of_study = display_field(_('Area of study:'), profile, :area_of_study)    
+    end
+
+    if lattes.blank? && fields_of_interest.blank? && schooling.blank? &&
+      area_of_study.blank? && formation.blank?
+      ''
+    else
+      content_tag('tr', content_tag('th', _('Academic information'), { :colspan => 2 })) + schooling + formation + area_of_study + fields_of_interest + lattes
+    end
+  end
+
   def display_work_info(profile)
     organization = display_field(_('Organization:'), profile, :organization)
     organization_site = display_field(_('Organization website:'), profile, :organization_website) { |url| link_to(url, url) }
